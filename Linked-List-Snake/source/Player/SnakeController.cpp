ad7f0f09c3be1419863c4cc6dccf774cd5a14f58
Rishabh Garg <narishabh.garg@gmail.com>
2024-09-07 16:16:03 +0530

    Setup Snake Controller

--- Linked-List-Snake/source/Player/SnakeController.cpp
+++ Linked-List-Snake/source/Player/SnakeController.cpp
@@ -2,13 +2,60 @@
 
 namespace Player
 {
-	SnakeController::SnakeController() = default;
+	SnakeController::SnakeController() { }
 
-	SnakeController::~SnakeController() = default;
+	SnakeController::~SnakeController()
+	{
+		destroy();
+	}
 
 	void SnakeController::initialize() { }
 
-	void SnakeController::update() { }
+	void SnakeController::update() 
+	{ 
+		switch (current_snake_state)
+		{
+		case SnakeState::ALIVE:
+			processPlayerInput();
+			updateSnakeDirection();
+			processSnakeCollision();
+			moveSnake();
+			break;
+
+		case SnakeState::DEAD:
+			handleRestart();
+			break;
+		}
+	}
 
 	void SnakeController::render() { }
+
+	void SnakeController::processPlayerInput() { }
+
+	void SnakeController::updateSnakeDirection() { }
+
+	void SnakeController::moveSnake() { }
+
+	void SnakeController::processSnakeCollision() { }
+
+	void SnakeController::handleRestart() { }
+
+	void SnakeController::spawnSnake() { }
+
+	void SnakeController::respawnSnake() { }
+
+	SnakeState SnakeController::getSnakeState() const
+	{
+		return current_snake_state;
+	}
+
+	void SnakeController::setSnakeState(SnakeState state)
+	{
+		current_snake_state = state;
+	}
+
+	void SnakeController::reset() { }
+
+	void SnakeController::destroy() { }
+
 }
